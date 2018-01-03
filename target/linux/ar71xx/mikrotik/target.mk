63f420fede92aaccbf647bb0cfdc09860e11eeb4
Gabor Juhos <juhosg@freemail.hu>
2018-01-03 10:16:37 +0100

    ar71xx: select minor feature
    It is required to build the kernel2minor utility which will
    be used to generate images for MikroTik boards with NOR flash.
    
    Signed-off-by: Gabor Juhos <juhosg@freemail.hu>

--- target/linux/ar71xx/mikrotik/target.mk
+++ target/linux/ar71xx/mikrotik/target.mk
@@ -1,5 +1,5 @@
 BOARDNAME:=Mikrotik devices with NAND flash
-FEATURES += targz ramdisk
+FEATURES += targz ramdisk minor
 
 define Target/Description
 	Build firmware images for Atheros AR71xx/AR913x based Mikrotik boards.
