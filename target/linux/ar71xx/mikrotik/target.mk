e2f304964e1d12288666bf3cb8e8ee8d7686aea3
Gabor Juhos <juhosg@freemail.hu>
2018-01-06 18:58:57 +0100

    ar71xx/mikrotik: add rbcfg to the default packages
    Signed-off-by: Gabor Juhos <juhosg@freemail.hu>

--- target/linux/ar71xx/mikrotik/target.mk
+++ target/linux/ar71xx/mikrotik/target.mk
@@ -1,6 +1,8 @@
 BOARDNAME:=Mikrotik devices with NAND flash
 FEATURES += targz ramdisk minor squashfs
 
+DEFAULT_PACKAGES += rbcfg
+
 define Target/Description
 	Build firmware images for Atheros AR71xx/AR913x based Mikrotik boards.
 	e.g. MikroTik RB-4xx or RB-750
